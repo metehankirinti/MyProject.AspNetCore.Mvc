12465bd19b52f8b1d44f14fb5e9c3dd291f712c0
Metehan Kırıntı <147526217+metehankirinti@users.noreply.github.com>
2024-01-03 14:56:32 +0300

    MyProject.AspNetCore.Mvc
    ASP.NET Core MVC ile araç alım satım uygulaması

---
+++ AspNetCore.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project_8.Models;
+
+namespace Project_8.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        aracEntities2 db = new aracEntities2();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Araclar()
+        {
+            var araclar = db.hancars.ToList();
+            return View(araclar);
+        }
+        public ActionResult AracDetay(int id)
+        {
+            var arac = db.hancars.FirstOrDefault(x => x.aracid == id);
+            ViewBag.data=arac;
+            return View("AracDetay",arac);
+        }
+
+        public ActionResult Marka()
+        {
+            var araclar = db.hancars.ToList();
+            return View(araclar);
+        }
+
+
+    }
+}
---
+++ AspNetCore.Mvc/Controllers/aracController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project_8.Models;
+
+namespace Project_8.Controllers
+{
+    public class aracController : Controller
+    {
+        // GET: arac
+        aracEntities2 db = new aracEntities2();
+        public ActionResult Index()
+        {
+            var araclar=db.hancars.ToList();
+            return View(araclar);
+        }
+        [HttpGet]
+        public ActionResult AracEkle ()
+        { 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AracEkle(hancars t)
+        {
+            db.hancars.Add(t);
+            db.SaveChanges();   
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Aracsil(int id)
+        {
+            var arac = db.hancars.Find(id);
+            db.hancars.Remove(arac);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Aracgüncelle(int id)
+        {
+            var araba = db.hancars.Find(id);
+            return View("Aracgüncelle", araba);
+        }
+        [HttpPost]
+        public ActionResult Aracgüncellee(hancars t)
+        {
+
+            var araba =db.hancars.Find(t.aracid);
+            araba.marka = t.marka;
+            araba.model = t.model;
+            araba.Renk = t.Renk;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
